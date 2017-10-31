6d851b4038b8203410a947dc031531f3006b9f41
Alex <katakunchik@gmail.com>
2017-10-31 16:40:25 +0200

    first commit

---
+++ DAL/Entities/ApplicationContext.cs
@@ -0,0 +1,31 @@
+﻿using DAL.Entities.Identity;
+using DAL.Interfaces;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class ApplicationContext: IdentityDbContext<ApplicationUser>, IApplicationContext
+    {
+        public ApplicationContext() : base("FinalDBConn")
+        {
+            Database.SetInitializer<ApplicationContext>(null);
+        }
+
+        public ApplicationContext(string connString)
+            : base(connString)
+        {
+            Database.SetInitializer<ApplicationContext>(new DBInitializer());
+        }
+
+        public new IDbSet<TEntity> Set<TEntity>() where TEntity : class
+        {
+            return base.Set<TEntity>();
+        }
+    }
+}
---
+++ DAL/Entities/DBInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    internal class DBInitializer : CreateDatabaseIfNotExists<ApplicationContext>
+    {
+        protected override void Seed(ApplicationContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
